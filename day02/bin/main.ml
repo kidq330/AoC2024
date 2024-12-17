1865897e361fe2707a895ce2928b19103d36d9b6
kidq330 <j.migdal42@gmail.com>
2024-12-17 10:38:08 +0100

    Rename variable in main.ml

--- day02/bin/main.ml
+++ day02/bin/main.ml
@@ -1,9 +1,9 @@
 open Day02
 
-let pair_lists = Io.parse_input () ;;
+let levels = Io.parse_input ();;
 
 (* regression: 411 *)
-BatPrintf.printf "Answer to first exercise: %d\n" (Ex1.solve pair_lists) ;;
+BatPrintf.printf "Answer to first exercise: %d\n" (Ex1.solve levels);;
 
 (* regression: 465 *)
-BatPrintf.printf "Answer to second exercise: %d\n" (Ex2.solve pair_lists)
+BatPrintf.printf "Answer to second exercise: %d\n" (Ex2.solve levels)
